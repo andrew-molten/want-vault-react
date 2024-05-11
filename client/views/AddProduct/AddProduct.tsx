a868b903765ee8154e3581f3eae4ea1dc7e53a26
Andrew Molten <andrewmolten23@gmail.com>
2024-05-12 10:11:07 +1200

    add the extra fields for addProduct

--- client/views/AddProduct/AddProduct.tsx
+++ client/views/AddProduct/AddProduct.tsx
@@ -2,11 +2,23 @@ import React, { useState } from 'react'
 
 interface Product {
   name: string
+  brand: string
+  price: number | undefined
+  willingTopay: number | undefined
+  productLink: string
+  pros: string
+  cons: string
 }
 
 function AddProduct() {
   const [product, setProduct] = useState<Product>({
     name: '',
+    brand: '',
+    price: undefined,
+    willingTopay: undefined,
+    productLink: '',
+    pros: '',
+    cons: '',
   })
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -18,7 +30,7 @@ function AddProduct() {
     <>
       <h2>Add Product</h2>
       <form>
-        <label htmlFor="name">What do you want?</label>
+        <label htmlFor="name">What do you want? </label>
         <input
           value={product.name}
           type="text"
@@ -26,6 +38,22 @@ function AddProduct() {
           id="name"
           onChange={handleChange}
         />
+        <br />
+        <label htmlFor="brand">Preferred brand: </label>
+        <input type="text" id="brand" name="brand" />
+        <br />
+        <label htmlFor="price">Current price: $</label>
+        <input type="text" id="price" name="price" /> <br />
+        <label htmlFor="willing-to-pay">
+          How much are you willing to pay?{' $'}
+        </label>
+        <input type="text" id="willing-to-pay" name="willingTopay" /> <br />
+        <label htmlFor="product-link">Link: </label>
+        <input type="text" id="product-link" name="productLink" /> <br />
+        <label htmlFor="pros">Pros: </label>
+        <input type="text" id="pros" name="pros" /> <br />
+        <label htmlFor="cons">Cons: </label>
+        <input type="text" id="cons" name="cons" /> <br />
       </form>
     </>
   )
